f446321372b76cedc2e4cc5ac7cb37a5bf5c91f9
Sergio <serginholdmf@gmail.com>
2026-04-09 22:54:29 -0300

    feat: add badge info dialog and gesture to display it in WorkoutSetRow

--- lib/features/treinos/widgets/executar_treino/workout_set_row.dart
+++ lib/features/treinos/widgets/executar_treino/workout_set_row.dart
@@ -21,6 +21,17 @@ class WorkoutSetRow extends StatelessWidget {
     required this.onCheck,
   });
 
+  void _showBadgeInfo(BuildContext context) {
+    final option = serieTypeOptions.firstWhere(
+      (opt) => opt.type == serie.tipo,
+      orElse: () => serieTypeOptions.last,
+    );
+    showDialog<void>(
+      context: context,
+      builder: (_) => _BadgeInfoDialog(option: option),
+    );
+  }
+
   String get _serieLabel {
     switch (serie.tipo) {
       case TipoSerie.aquecimento:
@@ -64,24 +75,29 @@ class WorkoutSetRow extends StatelessWidget {
           // Serie badge
           SizedBox(
             width: 36,
-            child: AnimatedContainer(
-              duration: const Duration(milliseconds: 280),
-              width: 30,
-              height: 30,
-              decoration: BoxDecoration(
-                color: isCompleted
-                    ? AppColors.primary.withAlpha(30)
-                    : _serieColor.withAlpha(22),
-                borderRadius: BorderRadius.circular(AppTheme.radiusSM),
-              ),
-              child: Center(
-                child: Text(
-                  _serieLabel,
-                  style: TextStyle(
-                    color: isCompleted ? AppColors.primary : _serieColor,
-                    fontSize: 13,
-                    fontWeight: FontWeight.w800,
-                    letterSpacing: -0.2,
+            child: GestureDetector(
+              onTap: serie.tipo != TipoSerie.trabalho
+                  ? () => _showBadgeInfo(context)
+                  : null,
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 280),
+                width: 30,
+                height: 30,
+                decoration: BoxDecoration(
+                  color: isCompleted
+                      ? AppColors.primary.withAlpha(30)
+                      : _serieColor.withAlpha(22),
+                  borderRadius: BorderRadius.circular(AppTheme.radiusSM),
+                ),
+                child: Center(
+                  child: Text(
+                    _serieLabel,
+                    style: TextStyle(
+                      color: isCompleted ? AppColors.primary : _serieColor,
+                      fontSize: 13,
+                      fontWeight: FontWeight.w800,
+                      letterSpacing: -0.2,
+                    ),
                   ),
                 ),
               ),
@@ -255,6 +271,81 @@ class _SetInputFieldState extends State<_SetInputField> {
             color: AppColors.labelTertiary,
             fontWeight: FontWeight.w500,
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class _BadgeInfoDialog extends StatelessWidget {
+  final SerieTypeOption option;
+
+  const _BadgeInfoDialog({required this.option});
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      backgroundColor: AppColors.surfaceDark,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(AppTheme.radiusXL),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              width: 52,
+              height: 52,
+              decoration: BoxDecoration(
+                color: option.color.withAlpha(28),
+                shape: BoxShape.circle,
+              ),
+              child: Icon(option.icon, color: option.color, size: 26),
+            ),
+            const SizedBox(height: 14),
+            Text(
+              option.title,
+              style: const TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.w700,
+                color: AppColors.labelPrimary,
+                letterSpacing: -0.3,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              option.subtitle,
+              style: const TextStyle(
+                fontSize: 14,
+                color: AppColors.labelSecondary,
+                height: 1.4,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 24),
+            GestureDetector(
+              onTap: () => Navigator.pop(context),
+              child: Container(
+                height: 46,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: AppColors.surfaceLight,
+                  borderRadius: BorderRadius.circular(AppTheme.radiusMD),
+                ),
+                child: const Center(
+                  child: Text(
+                    'Ok',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w600,
+                      color: AppColors.labelPrimary,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
