c2961407b3a1b85696a1a5b64bb42351e2f1aa2c
Sergio <serginholdmf@gmail.com>
2026-04-27 01:09:09 -0300

    refactor: remove unused Firebase Auth import in `main.dart`
    - Remove unused `package:firebase_auth/firebase_auth.dart` import from `lib/main.dart`

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:supabase_flutter/supabase_flutter.dart' as sb;
 import 'package:intl/date_symbol_data_local.dart';
